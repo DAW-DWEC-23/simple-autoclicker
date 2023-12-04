541e6928cd1e68048e20a6f98e54eb27811dce85
Jacinto Sanchez <j.sanchezvilla@edu.gva.es>
2023-12-04 09:07:28 +0100

    first commit

---
+++ src/ClickerGeneratorComponent.js
@@ -0,0 +1,140 @@
+'use strict';
+
+import Helper from "./ClickerHelper.js";
+
+export default class ClickerGenerator extends HTMLElement {
+
+  // Attributes
+  static G_ID = 'data-id';
+  static G_TITLE = 'data-title';
+  static G_PRICE = 'data-price';
+  static G_LEVEL = 'data-level';
+  static G_MPS = 'data-mps';
+  static G_COOLDOWN = 'data-cooldown';
+  
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+
+    this.gId = this.getAttribute(ClickerGenerator.G_ID);
+    this.gTitle = this.getAttribute(ClickerGenerator.G_TITLE);
+    this.gPrice = parseInt(this.getAttribute(ClickerGenerator.G_PRICE));
+    this.gLevel = parseInt(this.getAttribute(ClickerGenerator.G_LEVEL));
+    this.gMps = parseInt(this.getAttribute(ClickerGenerator.G_MPS));
+    this.gCooldown = parseInt(this.getAttribute(ClickerGenerator.G_COOLDOWN));
+    this.bankSavings = 0;
+  }
+  
+  connectedCallback() {
+    this.render();
+    this.style.display = "none";
+    if (this.gLevel > 0) this.resetProgressBar();
+    document.addEventListener(Helper.MsgBankUpdate, this);
+  }
+
+  // Event management
+
+  handleEvent(event) {
+    if (event.type === Helper.MsgBankUpdate) {
+      this.bankSavings = event.detail.bankSavings;
+      this.updateGeneratorVisibility();
+      this.updateUpgradeButtonAvailable();
+    }
+  }
+
+  // Custom event messages
+
+  sendQuantityToBank() {
+    const eventMessage = new CustomEvent(Helper.MsgBankAdd, {
+      detail: {
+        quantity: this.gMps * this.gLevel,
+      },
+      bubbles: true,
+      composed: true,
+    });
+    this.dispatchEvent(eventMessage);
+    this.resetProgressBar();
+  }
+
+  spendFromTheBank(cost) {
+    const eventMessage = new CustomEvent(Helper.MsgBankSubtract, {
+      detail: {
+        quantity: cost,
+      },
+      bubbles: true,
+      composed: true,
+    });
+    this.dispatchEvent(eventMessage);
+  }
+
+  // Render
+
+  render() {
+    this.shadowRoot.innerHTML = /*html*/ `
+      <style>
+        @import './src/styles/clicker-generator.scss';
+      </style>
+      <div>
+        <h3>${this.gTitle}</h3>
+        <p class="clicker-generator-level">Level ${this.gLevel}</p>
+        <div class="clicker-generator-progress-bar">
+          <div class="clicker-generator-progress"></div>
+        </div>
+        <p class="clicker-generator-mps">Income ${Helper.formatAmount(this.gMps * this.gLevel)} 💰/s</p>
+        <button class='clicker-generator-add-btn' disabled>Improve 💸 ${Helper.formatAmount(this.getCurrentCost())}</button>
+      </div>
+    `;
+
+    this.shadowRoot.querySelector(".clicker-generator-add-btn").addEventListener("click", () => this.upgrade() );
+  }
+
+  // View logic
+
+  updateGeneratorVisibility() {
+    if (this.bankSavings >= this.gPrice)
+      this.style.display = "inline-block";
+  }
+
+  updateUpgradeButtonAvailable() {
+    const upgradeBtn = this.shadowRoot.querySelector(".clicker-generator-add-btn")
+    if (this.bankSavings >= this.getCurrentCost())
+      upgradeBtn.removeAttribute("disabled");
+    else
+      upgradeBtn.setAttribute("disabled", "");
+  }
+
+  upgrade() {
+    this.spendFromTheBank(this.getCurrentCost());
+    this.gLevel++;
+    this.shadowRoot.querySelector(".clicker-generator-level").textContent = `Level ${this.gLevel}`;
+    this.shadowRoot.querySelector(".clicker-generator-mps").textContent = `Income ${Helper.formatAmount(this.gMps * this.gLevel)} 💰/s`;
+    this.shadowRoot.querySelector(".clicker-generator-add-btn").textContent = `Improve 💸 ${Helper.formatAmount(this.getCurrentCost())}`;
+    this.resetProgressBar();
+  }
+
+  getCurrentCost() {
+    return this.gPrice * 1.5 ** this.gLevel;
+  }
+
+  resetProgressBar() {
+    clearInterval(this.cooldownTimer);
+    this.cooldownTimer = null;
+    this.currentCooldownTime = 0;
+    this.cooldownTimer = setInterval(() => {
+      this.updateProgressBar();
+      this.currentCooldownTime += 30;
+    }, 30);
+  }
+
+  updateProgressBar() {
+    const progress = this.shadowRoot.querySelector(".clicker-generator-progress");
+    const progressValue = (this.currentCooldownTime) / this.gCooldown;
+    progress.style.width = `${progressValue * 100}%`;
+    if (progressValue >= 1) {
+      this.sendQuantityToBank();
+    }
+  }
+
+}
+
+customElements.define("clicker-generator", ClickerGenerator);
---
+++ src/clicker-data.js
@@ -0,0 +1,122 @@
+export const generators = [
+  {
+    id: 1,
+    title: "🏃🏻‍♂️",
+    price: 1,
+    level: 1,
+    mps: 1,
+    cooldown: 500
+  },
+  {
+    id: 2,
+    title: "🛴",
+    price: 10,
+    level: 0,
+    mps: 3,
+    cooldown: 600
+  },
+  {
+    id: 3,
+    title: "🚲",
+    price: 100,
+    level: 0,
+    mps: 10,
+    cooldown: 700
+  },
+  {
+    id: 4,
+    title: "🛵",
+    price: 1000,
+    level: 0,
+    mps: 100,
+    cooldown: 800
+  },
+  {
+    id: 5,
+    title: "🏍️",
+    price: 10_000,
+    level: 0,
+    mps: 1000,
+    cooldown: 900
+  },
+  {
+    id: 6,
+    title: "🚗",
+    price: 100_000,
+    level: 0,
+    mps: 10_000,
+    cooldown: 1000
+  },
+  {
+    id: 7,
+    title: "🚛",
+    price: 1_000_000,
+    level: 0,
+    mps: 100_000,
+    cooldown: 1100
+  },
+  {
+    id: 8,
+    title: "🚂",
+    price: 10_000_000,
+    level: 0,
+    mps: 1_000_000,
+    cooldown: 1200
+  },
+  {
+    id: 9,
+    title: "🚅",
+    price: 100_000_000,
+    level: 0,
+    mps: 10_000_000,
+    cooldown: 1300
+  },
+  {
+    id: 10,
+    title: "🛥️",
+    price: 1_000_000_000,
+    level: 0,
+    mps: 100_000_000,
+    cooldown: 1400
+  },
+  {
+    id: 11,
+    title: "🚤",
+    price: 10_000_000_000,
+    level: 0,
+    mps: 1_000_000_000,
+    cooldown: 1500
+  },
+  {
+    id: 12,
+    title: "🚢",
+    price: 100_000_000_000,
+    level: 0,
+    mps: 10_000_000_000,
+    cooldown: 1600
+  },
+  {
+    id: 13,
+    title: "🛩",
+    price: 1_000_000_000_000,
+    level: 0,
+    mps: 100_000_000_000,
+    cooldown: 1700
+  },
+  {
+    id: 14,
+    title: "✈",
+    price: 10_000_000_000_000,
+    level: 0,
+    mps: 1_000_000_000_000,
+    cooldown: 1800
+  },
+  {
+    id: 15,
+    title: "🚀",
+    price: 100_000_000_000_000,
+    level: 0,
+    mps: 10_000_000_000_000,
+    cooldown: 1800
+  }
+];
